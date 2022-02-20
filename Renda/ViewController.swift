4a7f6929bbd6b881db0d9a29334a3e650403dd08
yona kin <yonakin22@gmail.com>
2022-02-21 01:37:42 +0900

    firebase

--- Renda/ViewController.swift
+++ Renda/ViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Firebase
 
 class ViewController: UIViewController {
     
@@ -24,12 +25,41 @@ class ViewController: UIViewController {
         //ボタンを丸くする
         tapButton.layer.cornerRadius = 125
         
+        // Firestoreのデータを読み込む
+               firestore.collection("counts").document("share").addSnapshotListener { snapshot, error in
+                   if error != nil {
+                       print("エラーが発生しました")
+                       print(error)
+                       return
+                   }
+                   let data = snapshot?.data()
+                   if data == nil {
+                       print("データがありません")
+                       return
+                   }
+                   let count = data!["count"] as? Int
+                   if count == nil {
+                       print("countという対応する値がありません")
+                       return
+                   }
+                   self.tapCount = count!
+                   self.countLabel.text = String(count!)
+               }
+        
     }
     
     
+    // Firestoreを扱うためのプロパティ
+    let firestore = Firestore.firestore()
+
+    
     @IBAction func tapTapButton(){
-        tapCount = tapCount + 1
+        // タップを数える変数にプラス1する
+        tapCount += 1
+        // タップされた数をLabelに反映する
         countLabel.text = String(tapCount)
+        // FirestoreにtapCountを書き込む
+        firestore.collection("counts").document("share").setData(["count": tapCount])
     }
 
 
